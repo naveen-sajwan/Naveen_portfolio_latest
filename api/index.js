1929b4557b162aadbf4b18090b6e4164638c1397
Naveen Sajwan <naveensajwan@Naveens-MacBook-Air.local>
2026-04-02 20:22:43 +0530

    fixing backend

--- api/index.js
+++ api/index.js
@@ -1,9 +1,3 @@
 import app from "../server/index.js"
-import cors from 'cors'
-
-app.use(cors({
-  origin: 'https://naveen-portfolio-7hxysqvzq-naveens-projects-dc93ceec.vercel.app',
-  credentials: true
-}));
 
 export default app;
