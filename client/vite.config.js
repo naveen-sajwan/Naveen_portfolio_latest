5656fc508252db44a8f5fc149990b71525601f26
Naveen Sajwan <naveensajwan@Naveens-MacBook-Air.local>
2026-04-02 19:34:58 +0530

    i am in the flow

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import path from "path"
 
 export default defineConfig({
   plugins: [react()],
+  base: '/client/dist/',  // This tells Vite to prefix all asset paths
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
